fa3501eebff20a20dcd4819452eab797bbb4336c
Xander Endre <shadyalexcodes@gmail.com>
2021-10-12 10:18:12 -0600

    Created todo list

--- app/src/main/java/com/example/helloyou_simplegui/MainActivity.java
+++ app/src/main/java/com/example/helloyou_simplegui/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloyou_simplegui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+// Utilize this library for the view class: https://developer.android.com/reference/android/view/View
 import android.view.View;
 
 /**
@@ -24,6 +26,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    /*
+     * TODO LIST:
+     *  [x] Create a new project in Android Studio
+     *  [x] Choose an Empty Activity
+     *  [] Add three Plain Text (EditText) widgets to your layout
+     *  [] The first should hint for the user's first name
+     *  [] The second should hint for the user's last name
+     *  [] The third one will be used for output
+     *  [] Add a button widget with an event handler
+     *  [] When the button is pressed, the following should happen:
+     *      [] The text from the first name box should be put in a string variable called 'firstName'
+     *      [] The text from the last name box should be put in a string variable called 'lastName'
+     *      [] A new string variable called 'greeting' should be created
+     *          [] Set the value of this variable as a concatenation of "Hello ", firstName and lastName
+     *      [] Put the value of 'greeting' into the third Plain Text (EditText) widget
+     */
+
+    /* Event Handler */
     public void onClick(View view) {
         System.out.println("Hello!");
     }
