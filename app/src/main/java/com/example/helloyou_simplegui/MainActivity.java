06e2eb1c643849b5af7b4245608531e494cb6d2d
Xander Endre <shadyalexcodes@gmail.com>
2021-10-12 10:13:08 -0600

    Updated description

--- app/src/main/java/com/example/helloyou_simplegui/MainActivity.java
+++ app/src/main/java/com/example/helloyou_simplegui/MainActivity.java
@@ -5,6 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ *
+ * @name Hello, You! Simple GUI Activity
+ * @description Console output is an important tool to have as a developer that uses a console/text interface. Most applications we deal with daily use some form of a Graphical User Interface or GUI. This means it has its images, buttons, windows, and many of the features we associate with a typical app or program. In this exercise, we will build our first, simple GUI.
+ * @author Xander Endre
+ * @date 12 October 2021
+ * @class CSC110
+ *
+ */
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,12 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    /**
-     *
-     *
-     *
-     */
 
     public void onClick(View view) {
         System.out.println("Hello!");
