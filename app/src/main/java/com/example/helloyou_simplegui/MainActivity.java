cb324474f3e2bb08df81432a4847b9eeb96ecbbd
Xander Endre <shadyalexcodes@gmail.com>
2021-10-12 10:09:27 -0600

    Added onClick event handler that'll interact with a button printing 'Hello World'

--- app/src/main/java/com/example/helloyou_simplegui/MainActivity.java
+++ app/src/main/java/com/example/helloyou_simplegui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloyou_simplegui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     *
+     *
+     *
+     */
+
+    public void onClick(View view) {
+        System.out.println("Hello!");
     }
 }
